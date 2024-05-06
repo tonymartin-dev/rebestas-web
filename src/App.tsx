962c9e4630ed80533d2b21eec53f35a161499b95
Tony Martin <hello@tonymartin-web.com>
2024-05-06 08:01:20 +0200

    form test

--- src/App.tsx
+++ src/App.tsx
@@ -18,6 +18,32 @@ function App() {
 
       <section id="contact">
         <h1>Contacta con nosotros</h1>
+
+        <form name="contact" method="POST" data-netlify="true">
+          <input
+            type="hidden"
+            name="subject"
+            value="Message from Rebestas Form"
+          />
+          <p>
+            <label>
+              Your Name: <input type="text" name="name" />
+            </label>
+          </p>
+          <p>
+            <label>
+              Your Email: <input type="email" name="email" />
+            </label>
+          </p>
+          <p>
+            <label>
+              Message: <textarea name="message"></textarea>
+            </label>
+          </p>
+          <p>
+            <button type="submit">Send</button>
+          </p>
+        </form>
       </section>
     </>
   )
